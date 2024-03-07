cdae967090dce0ae98e806d01af22a8b8e649f47
mainbsl4 <mainbsl4@gmail.com>
2024-03-07 22:14:13 +0600

    main

--- src/views/Home/Banner.jsx
+++ src/views/Home/Banner.jsx
@@ -1,23 +1,21 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import '../../assets/css/banner.css';
+import "../../assets/css/banner.css";
 
 // import required modules
-import { Pagination, Navigation } from 'swiper/modules';
-
-
+import { Pagination, Navigation } from "swiper/modules";
 
 export default function Banner() {
   return (
     <>
-     <Swiper
+      <Swiper
         slidesPerView={1}
         spaceBetween={30}
         loop={true}
